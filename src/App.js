5f9b8fc80874ba6a48858c843b875398763fc28e
sbaiti <zouhaier~@gmailcom>
2020-07-27 20:11:43 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      name:"sbaiti"
+    this.state = {
+      name: "sbaiti",
+      surname: "sbaiti"
     }
   }
-  render(){
+  render() {
     console.log(this.state.name);
     console.log(this.state.name);
-
-  return (
-    <div className="App">
-      {this.state.name}
-    </div>
-  );
-}
+    let a = "";
+    {
+      this.state.name ?
+        a = "tal" : this.state.surname ? a = "tloul" : a = "talgg"
+    }
+    console.log(a);
+    return (
+      <div className="App">
+        {this.state.name}
+      </div>
+    );
+  }
 }
 
 export default App;
