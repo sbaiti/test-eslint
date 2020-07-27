fa35b73c62796d26fffbc742ad19cb6c4465e43f
sbaiti <zouhaier~@gmailcom>
2020-07-27 19:09:02 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      name="sbaiti"
+    }
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
+}
 }
 
 export default App;
